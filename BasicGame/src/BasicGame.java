a437df27546ccea6511f2f6006a3cc81f697ea77
Ergim <ergimc03@gmail.com>
2023-12-04 13:47:09 +0100

    2 dummies aangemaakt van de klasse god en de hp en naam van de karakter laten printen op de canvas

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -10,27 +10,49 @@ public class BasicGame implements GameLoop {
         SaxionApp.startGameLoop(new BasicGame(), 1500, 750, 40);
     }
 
+    God dummy = new God();
+    God dummy2 = new God();
+
+
     @Override
     public void init() {
-        SaxionApp.printLine("test");
-        SaxionApp.printLine("gfruy");
-        SaxionApp.printLine("Hamburger.");
+
+        dummy.characterId = 1;
+        dummy.hp = 100;
+        dummy.name = "Zeus";
+        dummy.abilityDamage1 = 20;
+
+        dummy2.characterId = 2;
+        dummy2.hp = 100;
+        dummy2.name = "Odin";
+        dummy2.abilityDamage1 = 25;
+
     }
 
     @Override
     public void loop() {
+        SaxionApp.drawText(dummy.name, 100, 100, 50);
+        SaxionApp.drawText(String.valueOf(dummy.hp),100,200,50);
+        SaxionApp.drawText(dummy2.name, 1300, 100,50);
+        SaxionApp.drawText(String.valueOf(dummy2.hp),1300,200,50);
+
+
+
 
     }
 
     @Override
     public void keyboardEvent(KeyboardEvent keyboardEvent) {
 
+
     }
 
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
 
+
     }
+
 }
 
 
