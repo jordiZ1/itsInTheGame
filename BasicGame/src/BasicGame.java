7764659a397334d2fc93a37428e663b4495648da
Jordi <jordizweden@gmail.com>
2023-12-04 13:47:39 +0100

    turn system

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -4,23 +4,35 @@ import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
+import java.util.ArrayList;
+
 public class BasicGame implements GameLoop {
 
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
     }
 
+    ArrayList<String> players = new ArrayList<>();
+    int turn;
+
     @Override
     public void init() {
-        SaxionApp.printLine("test");
-        SaxionApp.printLine("gfruy");
-        SaxionApp.printLine("Hamburger.");
-        SaxionApp.printLine("test git");
+        turn = 1;
+        players.add("Player 1");
+        players.add("Player 2");
     }
 
     @Override
     public void loop() {
+        SaxionApp.clear();
+        String currentPlayer = getCurrentPlayer();
 
+        SaxionApp.drawText("Turn: " + turn, 80, 80, 20);
+        SaxionApp.drawText(currentPlayer, 100, 100, 20);
+
+        SaxionApp.sleep(2);
+
+        turn++;
     }
 
     @Override
@@ -31,6 +43,14 @@ public class BasicGame implements GameLoop {
     @Override
     public void mouseEvent(MouseEvent mouseEvent) {
 
+    }
+
+    private String getCurrentPlayer() {
+        if (turn % 2 == 1) {
+            return players.get(0);
+        }
+
+        return players.get(1);
     }
 }
 
