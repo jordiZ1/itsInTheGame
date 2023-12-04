d66d87a398230c789198da8be364be1c68a88f02
yusuf <yusufsimsek52@outlook.com>
2023-12-04 13:50:04 +0100

    background

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -4,22 +4,28 @@ import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
+
 public class BasicGame implements GameLoop {
 
     public static void main(String[] args) {
-        SaxionApp.startGameLoop(new BasicGame(), 1000, 1000, 40);
+        SaxionApp.startGameLoop(new BasicGame(), 1500, 750, 40);
     }
+
+    public void backGroundImage() {
+
+    }
+
 
     @Override
     public void init() {
-        SaxionApp.printLine("test");
-        SaxionApp.printLine("gfruy");
-        SaxionApp.printLine("Hamburger.");
+
+
     }
 
     @Override
     public void loop() {
-
+        SaxionApp.drawImage("BasicGame/BattleArena1.jpg", 0,0, 1500, 750);
+        backGroundImage();
     }
 
     @Override
