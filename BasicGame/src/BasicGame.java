afca5d7c6c591420f1ccc3481f48fe97d191a73b
jordiZ1 <jordizweden@gmail.com>
2023-12-05 20:08:59 +0100

    restructure

--- BasicGame/src/BasicGame.java
+++ BasicGame/src/BasicGame.java
@@ -24,29 +24,8 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void loop() {
-        SaxionApp.clear();
-        SaxionApp.drawImage("BasicGame/BattleArena1.jpg", 0,0, 1500, 750);
+        drawGameBoard();
 
-        SaxionApp.drawImage("BasicGame/redAbility.png",130,580,110,200);
-        SaxionApp.drawImage("BasicGame/greenAbility.png",250,640,135,80);
-        SaxionApp.drawImage("BasicGame/blueAbility.png",395,640,110,80);
-
-        SaxionApp.drawImage("BasicGame/redAbility.png",1260,580,110,200);
-        SaxionApp.drawImage("BasicGame/greenAbility.png",1115,640,135,80);
-        SaxionApp.drawImage("BasicGame/blueAbility.png",995,640,110,80);
-
-        SaxionApp.drawImage("BasicGame/healthBar.png",200,-50,360,190);
-        SaxionApp.drawImage("BasicGame/healthBar.png",920,-50,360,190);
-
-        SaxionApp.drawImage("BasicGame/redCard.png",50,130,80,120);
-        SaxionApp.drawImage("BasicGame/greenCard.png/",50,320,80,120);
-        SaxionApp.drawImage("BasicGame/blueCard.png",50,510,80,120);
-
-        SaxionApp.drawImage("BasicGame/redCard.png",1370,130,80,120);
-        SaxionApp.drawImage("BasicGame/greenCard.png",1370,320,80,120);
-        SaxionApp.drawImage("BasicGame/blueCard.png",1370,510,80,120);
-
-        SaxionApp.clear();
         String currentPlayer = getCurrentPlayer();
 
         SaxionApp.drawText("Turn: " + turn, 80, 80, 20);
@@ -73,5 +52,29 @@ public class BasicGame implements GameLoop {
         }
 
         return players.get(1);
+    }
+
+    private void drawGameBoard() {
+        SaxionApp.clear();
+        SaxionApp.drawImage("BasicGame/BattleArena1.jpg", 0,0, 1500, 750);
+
+        SaxionApp.drawImage("BasicGame/redAbility.png",130,580,110,200);
+        SaxionApp.drawImage("BasicGame/greenAbility.png",250,640,135,80);
+        SaxionApp.drawImage("BasicGame/blueAbility.png",395,640,110,80);
+
+        SaxionApp.drawImage("BasicGame/redAbility.png",1260,580,110,200);
+        SaxionApp.drawImage("BasicGame/greenAbility.png",1115,640,135,80);
+        SaxionApp.drawImage("BasicGame/blueAbility.png",995,640,110,80);
+
+        SaxionApp.drawImage("BasicGame/healthBar.png",200,-50,360,190);
+        SaxionApp.drawImage("BasicGame/healthBar.png",920,-50,360,190);
+
+        SaxionApp.drawImage("BasicGame/redCard.png",50,130,80,120);
+        SaxionApp.drawImage("BasicGame/greenCard.png/",50,320,80,120);
+        SaxionApp.drawImage("BasicGame/blueCard.png",50,510,80,120);
+
+        SaxionApp.drawImage("BasicGame/redCard.png",1370,130,80,120);
+        SaxionApp.drawImage("BasicGame/greenCard.png",1370,320,80,120);
+        SaxionApp.drawImage("BasicGame/blueCard.png",1370,510,80,120);
     }
 }
