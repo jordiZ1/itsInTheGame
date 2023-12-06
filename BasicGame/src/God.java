91d9a5db528fc0523fd69d711004f0db6c2f9790
Ergim <ergimc03@gmail.com>
2023-12-06 10:22:36 +0100

    god klasse die nog miste

---
+++ BasicGame/src/God.java
@@ -0,0 +1,11 @@
+public class God {
+    int characterId;
+    String name = "Zeus";
+    int hp = 100;
+    int abilityDamage1;
+    int abilityDamage2;
+    int abilityDamage3;
+    String image;
+
+}
+
